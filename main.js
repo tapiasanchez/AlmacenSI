0de6ef5da3c407e8675fa207a3d587cf7e8e454e
Maria Tapia <201010007@est.umss.edu>
2023-09-12 12:24:14 -0400

    mi primer commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL('http://localhost:3000'); // Carga la aplicación React
+
+  mainWindow.on('closed', function () {
+    app.quit();
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
